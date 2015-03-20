b64cba1a2a434d059a49b342c74b80cdc834c3e7
Keith Reynolds <strange.aspiration@gmail.com>
2015-03-20 01:39:53 +0000

    Adds minified phaser

--- application.js
+++ application.js
@@ -1,1 +1,11 @@
 // Application.js
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game-space', { preload: preload, create: create, update: update });
+
+function preload() {
+}
+
+function create() {
+}
+
+function update() {
+}
