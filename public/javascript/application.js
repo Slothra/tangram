12c0eb82f9b4a0789878042f5eb4653d8e0ed2d5
heatherjang <heather.m.jang@gmail.com>
2015-03-20 03:18:00 +0000

    Added basic Sinatra framework

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -2,9 +2,11 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game-space', { preload: preload, create: create, update: update });
 
 function preload() {
+   game.load.image('brick', 'assets/sprites/block.png')
 }
 
 function create() {
+  var test = game.add.sprite(200, 200, 'brick')
 }
 
 function update() {
