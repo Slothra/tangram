7f4f564b883d6146f26c14d84b88958392fce348
Keith Reynolds <strange.aspiration@gmail.com>
2015-03-20 06:06:59 +0000

    adds spritesheet and movement animation

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -2,14 +2,13 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game-space', { preload: preload, create: create, update: update });
 
 function preload() {
-  game.load.image('brick', 'assets/sprites/block.png')
+  game.load.image('sky', 'assets/sky.png');
+  game.load.image('ground', 'assets/platform.png');
+  game.load.spritesheet('brick', 'assets/sprites/tan-square-move.png', 33, 37, 3);
+
 }
 
 function create() {
-  var test = game.add.sprite(200, 200, 'brick')
-  game.load.image('sky', 'assets/sky.png');
-  game.load.image('ground', 'assets/platform.png');
-  game.load.image('tan', 'assets/sprites/block.png');
 //  We're going to be using physics, so enable the Arcade Physics system
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
@@ -30,7 +29,58 @@ function create() {
 
     //  This stops it from falling away when you jump on it
     ground.body.immovable = true;
+    var ledge = platforms.create(400, 400, 'ground');
+
+    ledge.body.immovable = true;
+
+    ledge = platforms.create(-150, 250, 'ground');
+
+    ledge.body.immovable = true;
+
+    player = game.add.sprite(32, game.world.height - 100, 'brick')
+    game.physics.arcade.enable(player);
+
+    //  Player physics properties. Give the little guy a slight bounce.
+    player.body.bounce.y = 0.2;
+    player.body.gravity.y = 300;
+    player.body.collideWorldBounds = true;
+
+    player.animations.add('walk', [0, 1, 2], 10, true);
 }
 
 function update() {
+    //  Collide the player and the stars with the platforms
+    game.physics.arcade.collide(player, platforms);
+
+    cursors = game.input.keyboard.createCursorKeys();
+    //  Reset the players velocity (movement)
+    player.body.velocity.x = 0;
+
+    if (cursors.left.isDown)
+    {
+        //  Move to the left
+        player.body.velocity.x = -150;
+        player.animations.play('walk');
+    }
+    else if (cursors.right.isDown)
+    {
+        //  Move to the right
+        player.body.velocity.x = 150;
+        player.animations.play('walk');
+
+    }
+    else
+    {
+        //  Stand still
+        player.animations.stop();
+
+        player.frame = 0;
+
+    }
+    
+    //  Allow the player to jump if they are touching the ground.
+    if (cursors.up.isDown && player.body.touching.down)
+    {
+        player.body.velocity.y = -200;
+    }
 }
