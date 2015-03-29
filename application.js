7efccec6839784c1d5cfbc988402921878820d04
Keith Reynolds <strange.aspiration@gmail.com>
2015-03-29 01:14:58 +0000

    adds public folder

--- application.js
+++ application.js
@@ -2,7 +2,7 @@ var express = require('express')
 
 var app = express();
 
-// app.use(express.static('public'));
+app.use(express.static('public'));
 var port = process.env.PORT || 8080;
 
 app.get('/', function (req, res) {
