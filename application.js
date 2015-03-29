ad4067738161d824b965ae2a3b7ee8b938ec8654
Keith Reynolds <strange.aspiration@gmail.com>
2015-03-29 01:04:04 +0000

    Minor fixes

--- application.js
+++ application.js
@@ -2,7 +2,7 @@ var express = require('express')
 
 var app = express();
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
 app.get('/', function (req, res) {
 	res.send("<!DOCTYPE html> \
